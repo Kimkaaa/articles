1aadcbbc79958a67b1f56bb2c4bcf75f1286f4d9
kimkaaa <yami78999@gmail.com>
2024-10-15 15:07:55 +0900

    게시글 전체 보이기 및 개별 보이기 처리 완료

---
+++ src/main/java/com/my/articles/dto/ArticleDTO.java
@@ -0,0 +1,32 @@
+package com.my.articles.dto;
+
+import com.my.articles.entity.Article;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ArticleDTO {
+    private Long id;
+    private String title;
+    private String content;
+
+    public static ArticleDTO fromEntity(Article article){
+        return new ArticleDTO(
+                article.getId(),
+                article.getTitle(),
+                article.getContent());
+    }
+
+    public static Article fromDTO(ArticleDTO dto){
+        Article article = new Article();
+        article.setId(dto.getId());
+        article.setTitle(dto.getTitle());
+        article.setContent(dto.getContent());
+        return article;
+    }
+}
