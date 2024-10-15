60def0d82b19d1addf629525270195e9a9eefb26
Kimkaaa <yami78999@gmail.com>
2024-10-15 15:59:58 +0900

    게시글 수정, 삭제 외 추가로 입력까지 완성 (#7)

--- src/main/java/com/my/articles/controller/ArticleController.java
+++ src/main/java/com/my/articles/controller/ArticleController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -27,13 +28,15 @@ public class ArticleController {
     }
 
     @GetMapping("new")
-    public String newArticle() {
+    public String newArticle(Model model) {
+        model.addAttribute("dto", new ArticleDTO());
         return "/articles/new";
     }
 
     @PostMapping("create")
-    public String createArticle() {
-        return "redirect:articles";
+    public String createArticle(ArticleDTO dto) {
+        articleService.insertArticle(dto);
+        return "redirect:/articles";
     }
 
     @GetMapping("{id}")
@@ -44,17 +47,25 @@ public class ArticleController {
     }
 
     @GetMapping("{id}/update")
-    public String viewUpdateArticle() {
+    public String viewUpdateArticle(@PathVariable("id")Long id,
+                                    Model model) {
+        model.addAttribute("dto",
+                articleService.getOneArticle(id));
         return "/articles/update";
     }
 
     @PostMapping("update")
-    public String updateArticle() {
-        return "redirect:articles";
+    public String updateArticle(ArticleDTO dto) {
+        String url = "redirect:" + dto.getId();
+        articleService.updateArticle(dto);
+        return url;
     }
 
     @GetMapping("{id}/delete")
-    public String deleteArticle() {
-        return "redirect:articles";
+    public String deleteArticle(@PathVariable("id")Long id,
+                                RedirectAttributes redirectAttributes) {
+        articleService.deleteArticle(id);
+        redirectAttributes.addFlashAttribute("msg","정상적으로 삭제되었습니다.");
+        return "redirect:/articles";
     }
 }
