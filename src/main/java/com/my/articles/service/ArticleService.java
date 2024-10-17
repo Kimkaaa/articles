bbd72f468902cec6ddf3e7a599fe3e20dc84dfb9
kimkaaa <yami78999@gmail.com>
2024-10-17 16:57:26 +0900

    Paging 처리 완료

--- src/main/java/com/my/articles/service/ArticleService.java
+++ src/main/java/com/my/articles/service/ArticleService.java
@@ -4,7 +4,10 @@ import com.my.articles.dao.ArticleDAO;
 import com.my.articles.dto.ArticleDTO;
 import com.my.articles.entity.Article;
 import com.my.articles.entity.Comment;
+import com.my.articles.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -15,6 +18,9 @@ import java.util.List;
 public class ArticleService {
     @Autowired
     ArticleDAO dao;
+
+    @Autowired
+    ArticleRepository articleRepository;
 
     public List<ArticleDTO> getAllArticle() {
         List<Article> articles = dao.getAllArticle();
@@ -43,5 +49,10 @@ public class ArticleService {
 
     public void insertArticle(ArticleDTO dto) {
         dao.insertArticle(ArticleDTO.fromDTO(dto));
+    }
+
+    public Page<ArticleDTO> getArticlePage(Pageable pageable) {
+        Page<Article> articles = articleRepository.findAll(pageable);
+        return articles.map(x->ArticleDTO.fromEntity(x));
     }
 }
